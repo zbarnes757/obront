59fe49f6001da7deb7fcbf9366f1fc6d30bfc41b
Zac Barnes <zac.barnes89@gmail.com>
2016-03-04 18:37:17 -0600

    setup seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,52 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-unless User.any?
-  puts "Seeding users..."
-  User.create({
-    first_name:            "Zac",
-    last_name:             "Barnes",
-    email:                 "zac.barnes89@gmail.com",
-    admin:                 true,
-    password:              "123456",
-    password_confirmation: "123456"
-    })
+# unless User.any?
+#   puts "Seeding users..."
+#   User.create({
+#     first_name:            "Zac",
+#     last_name:             "Barnes",
+#     email:                 "zac.barnes89@gmail.com",
+#     admin:                 true,
+#     password:              "123456",
+#     password_confirmation: "123456"
+#     })
+#
+#   User.create({
+#     first_name:            "Joe",
+#     last_name:             "Editor",
+#     email:                 "joe.editor@gmail.com",
+#     admin:                 false,
+#     password:              "654321",
+#     password_confirmation: "654321"
+#     })
+#   puts "Users have been seeded."
+# end
 
-  User.create({
-    first_name:            "Joe",
-    last_name:             "Editor",
-    email:                 "joe.editor@gmail.com",
-    admin:                 false,
-    password:              "654321",
-    password_confirmation: "654321"
-    })
-  puts "Users have been seeded."
+# puts "Seeding categories..."
+# Category.find_or_create_by(pretty_name: "Business", name: "business")
+# Category.find_or_create_by(pretty_name: "Personal Finance", name: "personal_finance")
+# Category.find_or_create_by(pretty_name: "Sales & Marketing", name: "sales_and_marketing")
+# Category.find_or_create_by(pretty_name: "Career Development", name: "career_development")
+# Category.find_or_create_by(pretty_name: "Self Improvement", name: "self_improvement")
+# Category.find_or_create_by(pretty_name: "Health / Fitness", name: "health_fitness")
+# Category.find_or_create_by(pretty_name: "Science", name: "science")
+# Category.find_or_create_by(pretty_name: "Spirituality", name: "spirituality")
+# Category.find_or_create_by(pretty_name: "Sports", name: "sports")
+# Category.find_or_create_by(pretty_name: "Technology", name: "technology")
+# Category.find_or_create_by(pretty_name: "Memoir", name: "memoir")
+# Category.find_or_create_by(pretty_name: "Miscellaneous", name: "miscellaneous")
+# puts "Categories have been seeded."
+
+User.editors.each do |user|
+  if user.not_yet_assigned? || user.first_assignment? || user.b_list? || user.a_list? || user.all_star?
+    user.classification = 0
+  elsif user.a_list_outliner? || user.trial_period? || user.all_star_outliner?
+    user.classification = 1
+  elsif user.proof_reader?
+    user.classification = 2
+  elsif user.cover_designer?
+    user.classification = 3
+  end
+  user.save
 end
-
-puts "Seeding categories..."
-Category.find_or_create_by(pretty_name: "Business", name: "business")
-Category.find_or_create_by(pretty_name: "Personal Finance", name: "personal_finance")
-Category.find_or_create_by(pretty_name: "Sales & Marketing", name: "sales_and_marketing")
-Category.find_or_create_by(pretty_name: "Career Development", name: "career_development")
-Category.find_or_create_by(pretty_name: "Self Improvement", name: "self_improvement")
-Category.find_or_create_by(pretty_name: "Health / Fitness", name: "health_fitness")
-Category.find_or_create_by(pretty_name: "Science", name: "science")
-Category.find_or_create_by(pretty_name: "Spirituality", name: "spirituality")
-Category.find_or_create_by(pretty_name: "Sports", name: "sports")
-Category.find_or_create_by(pretty_name: "Technology", name: "technology")
-Category.find_or_create_by(pretty_name: "Memoir", name: "memoir")
-Category.find_or_create_by(pretty_name: "Miscellaneous", name: "miscellaneous")
-puts "Categories have been seeded."
