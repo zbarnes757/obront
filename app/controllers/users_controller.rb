dbbf8f99a8b4e7d5ef32553d54ffe775160c6162
Zac Barnes <zac.barnes89@gmail.com>
2016-08-07 14:45:44 -0500

    fix use of trello api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,8 @@ class UsersController < ApplicationController
     freelancer_board.cards.each do |card|
       if card.name == user.full_name
         card.add_comment(user.build_comment)
-        card.update!({ desc: user.build_description })
+        card.update({ desc: user.build_description })
+        card.save
         break
       end
     end
